e96215f07178a6f56c52866d8feab421f2cf860a
Santos2175 <santoshgurung949@gmail.com>
2025-05-14 05:03:55 +0545

    feat(backend,auth): auth model added

---
+++ backend/models/user.model.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// User Schema
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilImageUrl: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare password
+userSchema.methods.comparePassword(async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
