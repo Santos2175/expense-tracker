c3c89b82f2fcd1f18ac8aaaac191cf62fd0d0350
Santos2175 <santoshgurung949@gmail.com>
2025-05-14 08:33:18 +0545

    feat(backend, expense): delete expense and download expense API added

--- backend/controllers/expense.controller.js
+++ backend/controllers/expense.controller.js
@@ -1,3 +1,4 @@
+const xlsx = require('xlsx');
 const Expense = require('../models/expense.model');
 
 // Add expense handler
@@ -46,10 +47,52 @@ const getAllExpense = async (req, res) => {
 };
 
 // Delete expense
-const deleteExpense = async (req, res) => {};
+const deleteExpense = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Check if expense exists
+    const expense = await Expense.findById(id);
+
+    if (!expense) {
+      return res
+        .status(404)
+        .json({ success: false, message: `Expense not found` });
+    }
+
+    await Expense.findByIdAndDelete(id);
+
+    res
+      .status(200)
+      .json({ success: true, message: `Expense deleted successfully` });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
 
 // Download expense exel data handler
-const downloadExpenseExcel = async (req, res) => {};
+const downloadExpenseExcel = async (req, res) => {
+  const userId = req.user._id;
+
+  try {
+    const expenses = await Expense.find({ userId }).sort({ date: -1 });
+
+    // Prepare data for excel sheet
+    const data = expenses.map((item) => ({
+      Category: item.category,
+      Amount: item.amount,
+      Date: item.date,
+    }));
+
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(data);
+    xlsx.utils.book_append_sheet(wb, ws, 'Expense');
+    xlsx.writeFile(wb, 'expense_detail.xlsx');
+    res.download('expense_detail.xlsx');
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
 
 module.exports = {
   addExpense,
