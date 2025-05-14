c3d77209a9ca815fe53df4b997384201f6d7ca8d
Santos2175 <santoshgurung949@gmail.com>
2025-05-14 08:10:04 +0545

    feat(backend, expense): expense model added

---
+++ backend/models/expense.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+// Expense schema
+const expenseSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    icon: {
+      type: String,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps }
+);
+
+const Expense = mongoose.model('Expense', expenseSchema);
+
+module.exports = Expense;
